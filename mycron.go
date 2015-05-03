d8457bf202caab6470a320a3cbbd54ae4d87a6b0
wida <wida@foxmail.com>
2015-05-03 16:04:35 +0000

    修改项目结构

--- mycron.go
+++ mycron.go
@@ -18,20 +18,8 @@ func main() {
     for i := 0; i < len(jobs); i++ {
         job := jobs[i]
         c.AddFunc(job.Time,
-        func() {
-            defer func() {
-                if err := recover(); err != nil {
-                    mycron.Log(err);
-                }
-            }()
-            job.ChangeRunningStatu(1)
-            s, e := mycron.ExecWithTimeout(time.Second*10, job.Cmd)
-            if e != nil {
-                fmt.Print(e)
-            }
-            job.ChangeRunningStatu(0)
-            fmt.Println(s)
-        }, int(job.Status), int(job.ID), int64(job.STime), int64(job.ETime))
+        func() {jobrun(job)},
+        int(job.Status), int(job.ID), int64(job.STime), int64(job.ETime))
     }
     //start
     c.Start()
@@ -44,26 +32,30 @@ func main() {
             for i := 0; i < len(jobs); i++ {
                 job := jobs[i]
                 c.AddFunc(job.Time,
-                func() {
-                    defer func() {
-                        if err := recover(); err != nil {
-                            mycron.Log(err);
-                        }
-                    }()
-                    job.ChangeRunningStatu(1)
-                    s, e := mycron.ExecWithTimeout(time.Second*10, job.Cmd)
-                    if e != nil {
-                        fmt.Print(e)
-                    }
-                    job.ChangeRunningStatu(0)
-                    fmt.Println(s)
-                }, int(job.Status), int(job.ID), int64(job.STime), int64(job.ETime))
+                func() {jobrun(job)},
+                int(job.Status), int(job.ID), int64(job.STime), int64(job.ETime))
             }
             mycron.UpdateModifyList()
             continue
         }
     }
 }
+
+func jobrun(job mycron.Job){
+    defer func() {
+        if err := recover(); err != nil {
+            mycron.Log(err);
+        }
+    }()
+    job.ChangeRunningStatu(1)
+    s, e := mycron.ExecWithTimeout(time.Second*10, job.Cmd)
+    if e != nil {
+        fmt.Print(e)
+    }
+    job.ChangeRunningStatu(0)
+    fmt.Println(s)
+}
+
 /*
 func printfEntry(c *cron.Cron) {
     for _, v := range c.Entries() {
